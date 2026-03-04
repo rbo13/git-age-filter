33209b4961f5c1a5002b6def62db2e65b20f41bf
Richard Burk Orofeo <chardyy.orofeo@gmail.com>
2026-03-04 16:49:23 +0800

    bootstrap cli command to main

--- cmd/git-age-filter/main.go
+++ cmd/git-age-filter/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"context"
+	"io"
+	"os"
+
+	"github.com/rbo13/git-age-filter/internal/cli"
+)
+
 func main() {
-	println("Hello, world from main!")
+	os.Exit(run(os.Args[1:], os.Stdout, os.Stderr))
+}
+
+func run(args []string, stdout, stderr io.Writer) int {
+	return cli.Run(context.Background(), args, stdout, stderr)
 }
